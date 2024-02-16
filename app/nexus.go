8d4e7cd7129cea97f5b6b6c312468153ace46ae2
sackidude <nils.nilsen9@outlook.com>
2024-02-16 18:52:37 +0100

    Basic Post handling

--- app/nexus.go
+++ app/nexus.go
@@ -5,6 +5,7 @@ import (
 	"html/template"
 	"net/http"
 	"os"
+	"strconv"
 )
 
 // TODO: replace all _ with real errorhandling and fallback options
@@ -18,6 +19,7 @@ func main() {
 	// Preload fragments
 	fetcherHTML, _ := os.ReadFile("fragments/fetcher/main.html")
 
+	// HTTP Requests
 	imageRequest := func(w http.ResponseWriter, r *http.Request) {
 		// Request the relevant information to db.
 		directoryName := "trial-3"
@@ -25,6 +27,7 @@ func main() {
 		time := "16/2-24"
 		// Template html fragment with the information from db
 		tmpl, _ := template.New("t").Parse(string(fetcherHTML))
+
 		tmpl.Execute(w, map[string]string{
 			"trial": directoryName,
 			"image": filename,
@@ -40,6 +43,14 @@ func main() {
 		tmpl.Execute(w, nil)
 	}
 	http.HandleFunc("/data-view", dataViewer)
+
+	// HTTP Posts
+	imageDataRetrival := func(w http.ResponseWriter, r *http.Request) {
+		pxHeightString := r.Header.Get("pxHeight")
+		pxHeight, _ := strconv.ParseFloat(pxHeightString, 64)
+		fmt.Printf("User image data received with pxHeight: %.1f", pxHeight)
+	}
+	http.HandleFunc("/user-image-data", imageDataRetrival)
 
 	http.ListenAndServe("localhost:8080", nil)
 }
