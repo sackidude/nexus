eef0d5518dfc9a2943e3da1f5ce2310d68ce4e31
NilsJR <42474434+sackidude@users.noreply.github.com>
2024-02-16 12:45:26 +0000

    structure for whole project

--- app/nexus.go
+++ app/nexus.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
 	"net/http"
 )
@@ -11,6 +12,21 @@ func main() {
 		tmpl.Execute(w, nil)
 	}
 	http.HandleFunc("/", main)
+
+	// HTMX triggers
+	imageRequest := func(w http.ResponseWriter, r *http.Request) {
+		htmlStr := fmt.Sprintf("image nr. %d", 1)
+		tmpl, _ := template.New("t").Parse(htmlStr)
+		tmpl.Execute(w, nil)
+	}
+	http.HandleFunc("/image-request", imageRequest)
+
+	dataViewer := func(w http.ResponseWriter, r *http.Request) {
+		htmlStr := fmt.Sprintf("Here are the graphs %d", 1)
+		tmpl, _ := template.New("t").Parse(htmlStr)
+		tmpl.Execute(w, nil)
+	}
+	http.HandleFunc("/data-view", dataViewer)
 
 	http.ListenAndServe("localhost:8080", nil)
 }
