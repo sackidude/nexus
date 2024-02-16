0edbc88b8a20a117606bacdae8231155e4fcd113
sackidude <nils.nilsen9@outlook.com>
2024-02-16 19:39:55 +0100

    Temporary solution for fetcher, should change.

--- app/nexus.go
+++ app/nexus.go
@@ -10,6 +10,24 @@ import (
 
 // TODO: replace all _ with real errorhandling and fallback options
 
+var imgNum = 1
+
+func getNewImageData() map[string]string {
+	// TODO: Request data from db
+
+	directoryName := "trial-3"
+	filename := fmt.Sprintf("%d.jpg", imgNum)
+	imgNum++
+	time := "16/2-24"
+	//
+	return map[string]string{
+		"trial": directoryName,
+		"image": filename,
+		"time":  time,
+		"path":  fmt.Sprintf("images/%s/%s", directoryName, filename),
+	}
+}
+
 func main() {
 	// Basic stating hosting
 	fs := http.FileServer(http.Dir("./public"))
@@ -22,18 +40,10 @@ func main() {
 	// HTTP Requests
 	imageRequest := func(w http.ResponseWriter, r *http.Request) {
 		// Request the relevant information to db.
-		directoryName := "trial-3"
-		filename := "1.jpg"
-		time := "16/2-24"
 		// Template html fragment with the information from db
 		tmpl, _ := template.New("t").Parse(string(fetcherHTML))
-
-		tmpl.Execute(w, map[string]string{
-			"trial": directoryName,
-			"image": filename,
-			"time":  time,
-			"path":  fmt.Sprintf("images/%s/%s", directoryName, filename),
-		})
+		imageData := getNewImageData()
+		tmpl.Execute(w, imageData)
 	}
 	http.HandleFunc("/image-request", imageRequest)
 
@@ -49,6 +59,10 @@ func main() {
 		pxHeightString := r.Header.Get("pxHeight")
 		pxHeight, _ := strconv.ParseFloat(pxHeightString, 64)
 		fmt.Printf("User image data received with pxHeight: %.1f", pxHeight)
+
+		imageData := getNewImageData()
+		tmpl, _ := template.New("t").Parse(string(fetcherHTML))
+		tmpl.Execute(w, imageData) //TODO: Fix this temporary solution
 	}
 	http.HandleFunc("/user-image-data", imageDataRetrival)
 
