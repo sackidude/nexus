5a835525d732235b7b0e3c7651874dcc40ee85c8
sackidude <nils.nilsen9@outlook.com>
2024-03-16 15:14:18 +0100

    moving files to new destination

---
+++ static/scripts/fetcher.js
@@ -0,0 +1,7 @@
+/* document.getElementById("fetcher-img").addEventListener("mousemove", (e) => {
+  const line = document.getElementById("line");
+  const rect = document.getElementById("fetcher-img");
+  line.style.display = "block";
+  line.style.top = e.clientY + "px";
+  // console.log(rect);
+}); */
